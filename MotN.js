d35fddeba0d5a3158a2163da7960002c625a2324
Huff <90073132+PiSquaredx3@users.noreply.github.com>
2025-02-18 10:20:14 -0600

    Update MotN.js

--- MotN.js
+++ MotN.js
@@ -6,7 +6,8 @@ const sentences = [
     "Hi retrodev",
     "Ask me about Earthbound, please I need someone to talk to",
     "Neymar Jr. Add me on Steam, I don't care about Football I just like your csgo inventory",
-    "Bad Web Design? I'm not a spider."
+    "Bad Web Design? I'm not a spider.",
+    "'Peter, what are you doing?' 'Crack.' 'What the-'"
 ];
 
 
